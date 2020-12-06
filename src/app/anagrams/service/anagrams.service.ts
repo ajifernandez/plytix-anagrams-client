b46350d575ee9955cc16cf418000d727f5ee8c8c
Agustín Jiménez Fernández <agustinjf87@gmail.com>
2020-12-06 19:32:44 +0100

    Some comments

--- src/app/anagrams/service/anagrams.service.ts
+++ src/app/anagrams/service/anagrams.service.ts
@@ -12,14 +12,25 @@ export class AnagramsService {
 
   constructor(private httpClient: HttpClient) { }
 
+  /**
+   * Get all the words from the server
+   */
   public retrieveWords(): any {
     return this.httpClient.get<String[]>(`${this.apiAnagramsURL}/words`);
   }
 
+  /**
+   * Save the order of the words
+   * @param words the words sorted
+   */
   public saveWords(words: String[]) {
     return this.httpClient.post(`${this.apiAnagramsURL}/save`, words).subscribe();
   }
 
+  /**
+   * Get the list of words that fit with the anagram of the word
+   * @param searchValue word
+   */
   public getAnagram(searchValue: string) {
     return this.httpClient.post(`${this.apiAnagramsURL}/anagrams`, searchValue);
   }
